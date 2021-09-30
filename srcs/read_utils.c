2c4785d77af4793e55e35099e21c5f45641b2153
Joao Santos <jsantos@c2r1s6.42lisboa.com>
2021-09-30 16:36:56 +0100

    Rectangle done

--- srcs/read_utils.c
+++ srcs/read_utils.c
@@ -1,7 +1,7 @@
 #include "../headers/read_utils.h"
 
 static int	get_info(FILE *file, t_info *info);
-static int	get_op(FILE *file, t_info *info, t_op *op);
+static int	get_op(FILE *file, t_op *op);
 
 int	read_file_and_print(char *arg)
 {
@@ -16,7 +16,7 @@ int	read_file_and_print(char *arg)
 	if (!init_matrix(&matrix, info.height, info.width)
 		|| !fill_matrix(&matrix, info.height, info.width, info.bg_char))
 		return (0);
-	while (get_op(file, &info, &op))
+	while (get_op(file, &op))
 	{
 		if (op.fill_flag == 'R')
 			draw_full_shape(&info, &op, matrix);
@@ -24,9 +24,16 @@ int	read_file_and_print(char *arg)
 			draw_contour(&info, &op, matrix);
 	}
 	if (op.scan_ret == -1)
+	{
 		print_matrix(&info, matrix);
-	free_matrix(matrix, info.height);
-	return (fscanf(file, "%*d") == EOF);
+		free_matrix(matrix, info.height);
+		return (fscanf(file, "%*d") == EOF);
+	}
+	else
+	{
+		free_matrix(matrix, info.height);
+		return (0);
+	}
 }
 
 static int	get_info(FILE *file, t_info *info)
@@ -40,16 +47,13 @@ static int	get_info(FILE *file, t_info *info)
 	return (1);
 }
 
-static int	get_op(FILE *file, t_info *info, t_op *op)
+static int	get_op(FILE *file, t_op *op)
 {
 	op->scan_ret = fscanf(file, "%c %f %f %f %f %c\n", &op->fill_flag, &op->start_x,
 			   &op->start_y, &op->width, &op->height, &op->shape_char);
 	if (op->scan_ret != 6
-		|| (op->fill_flag != 'r' && op->fill_flag != 'R')
-		|| !is_in_range(op->start_x, 0, info->width)
-		|| !is_in_range(op->start_y, 0, info->height)
-		|| !is_in_range(op->width, 0, info->width) || op->width == 0
-		|| !is_in_range(op->height, 0, info->height) || op->height == 0)
+	|| (op->fill_flag != 'r' && op->fill_flag != 'R')
+	|| op->width < 0 || op->height < 0)
 		return (0);
 	return (1);
 }
